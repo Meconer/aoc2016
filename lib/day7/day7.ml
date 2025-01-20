27f2e569aa9a7373c52ad3a5612911bb396de8c8
Meconer <mats.asson@gmail.com>
2025-01-20 19:10:16 +0100

    WIP

--- lib/day7/day7.ml
+++ lib/day7/day7.ml
@@ -6,5 +6,14 @@ let filename =
   if isExample then "lib/day7/example.txt" else "lib/day7/input.txt"
 
 let aoc_input = In_channel.read_lines filename
+
+let supports_TLS line =
+  let rec loop in_bracket out_bracket line =
+  let fb_pos = String.index line '[' in
+  let sb_pos = String.index line ']' in
+  match fb_pos with 
+
+(* let solve_p1 lines =
+   List.count lines ~f:(fun line -> supports_TLS line) *)
 let resultP1 = 0
 let resultP2 = 0
