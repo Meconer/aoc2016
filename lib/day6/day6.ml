de5a2549edbfeb65930c3eb24646d8cda49f3c47
Meconer <mats.asson@gmail.com>
2025-01-20 17:13:51 +0100

    Day 6

--- lib/day6/day6.ml
+++ lib/day6/day6.ml
@@ -1,28 +1,80 @@
 open Core
 
-let isExample = true
+let isExample = false
 
 let filename =
   if isExample then "lib/day6/example.txt" else "lib/day6/input.txt"
 
 let aoc_input = In_channel.read_lines filename
 
+let get_most_used_char ch_list =
+  let list = List.sort ch_list ~compare:Char.compare in
+  let rec loop acc last_char count list =
+    match list with
+    | [] ->
+        let sorted =
+          List.sort acc ~compare:(fun a b -> Int.descending (snd a) (snd b))
+        in
+        fst (List.hd_exn sorted)
+    | ch :: rest ->
+        if Char.equal last_char ch then loop acc last_char (count + 1) rest
+        else loop ((last_char, count) :: acc) ch 1 rest
+  in
+  loop [] ' ' 0 list
 
-let parse_input lines =
-
+let solve_p1 lines =
   let length = String.length (List.hd_exn lines) in
-  let start_lists = List.init length ~f:(fun _ -> []) in
+  let char_lists = Array.create ~len:length [] in
 
-  let rec loop acc lines =
-  match lines with 
-  | [] -> List.rev acc
-  | line:: rest ->
-    let lst = String.to_list line in
-    let new_lists = List.foldi lst ~init:acc ~f:(fun idx ch ->
-       )
+  let rec loop lines =
+    match lines with
+    | [] -> char_lists
+    | line :: rest ->
+        let lst = String.to_list line in
+        List.iteri lst ~f:(fun idx ch ->
+            let curr = Array.get char_lists idx in
+            Array.set char_lists idx (ch :: curr));
+        loop rest
+  in
+  let lists = loop lines in
+  Array.map lists ~f:(fun list -> get_most_used_char list)
 
+let res = solve_p1 aoc_input
+let resultP1 = String.of_array res
 
+let get_least_used_char ch_list =
+  let list = List.sort ch_list ~compare:Char.compare in
+  let rec loop acc last_char count list =
+    match list with
+    | [] ->
+        let sorted =
+          List.sort ((last_char, count) :: acc) ~compare:(fun a b ->
+              Int.compare (snd a) (snd b))
+        in
+        let without_space = List.drop sorted 1 in
+        without_space
+    | ch :: rest ->
+        if Char.equal last_char ch then loop acc last_char (count + 1) rest
+        else loop ((last_char, count) :: acc) ch 1 rest
+  in
+  let lst = loop [] ' ' 0 list in
+  fst (List.hd_exn lst)
 
+let solve_p2 lines =
+  let length = String.length (List.hd_exn lines) in
+  let char_lists = Array.create ~len:length [] in
 
-let resultP1 = 0
-let resultP2 = 0
+  let rec loop lines =
+    match lines with
+    | [] -> char_lists
+    | line :: rest ->
+        let lst = String.to_list line in
+        List.iteri lst ~f:(fun idx ch ->
+            let curr = Array.get char_lists idx in
+            Array.set char_lists idx (ch :: curr));
+        loop rest
+  in
+  let lists = loop lines in
+  Array.map lists ~f:(fun list -> get_least_used_char list)
+
+let resultP2 = String.of_array (solve_p2 aoc_input)
