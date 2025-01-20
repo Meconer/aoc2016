24119f340801d92ce90ece6c3cf2b814cfd86184
Meconer <mats.asson@gmail.com>
2025-01-20 20:47:16 +0100

    Wip. Correct on example, wrong on input

--- lib/day7/day7.ml
+++ lib/day7/day7.ml
@@ -1,19 +1,44 @@
 open Core
 
-let isExample = true
+let isExample = false
 
 let filename =
   if isExample then "lib/day7/example.txt" else "lib/day7/input.txt"
 
 let aoc_input = In_channel.read_lines filename
 
+let has_rev_pair s =
+  let c_list = String.to_list s in
+  let rec loop c_list =
+    match c_list with
+    | [ _; _; _ ] | [ _; _ ] | _ :: [] | [] -> false
+    | a :: b :: c :: d :: rest ->
+        if Char.equal a d && Char.equal b c && not (Char.equal a b) then true
+        else loop (b :: c :: d :: rest)
+  in
+  loop c_list
+
 let supports_TLS line =
   let rec loop in_bracket out_bracket line =
-  let fb_pos = String.index line '[' in
-  let sb_pos = String.index line ']' in
-  match fb_pos with 
+    let fb_pos = String.index line '[' in
+    let sb_pos = String.index line ']' in
+    match fb_pos with
+    | None -> (in_bracket, out_bracket ^ line)
+    | Some idx -> (
+        match sb_pos with
+        | None -> failwith "No matching bracket"
+        | Some s_idx ->
+            let out_bracket = out_bracket ^ String.sub line ~pos:0 ~len:idx in
+            let in_bracket =
+              in_bracket ^ String.sub line ~pos:(idx + 1) ~len:(s_idx - idx - 1)
+            in
+            loop in_bracket out_bracket
+              (String.sub line ~pos:(s_idx + 1)
+                 ~len:(String.length line - s_idx - 1)))
+  in
+  let in_bracket, out_bracket = loop "" "" line in
+  if has_rev_pair in_bracket then false else has_rev_pair out_bracket
 
-(* let solve_p1 lines =
-   List.count lines ~f:(fun line -> supports_TLS line) *)
-let resultP1 = 0
+let solve_p1 lines = List.count lines ~f:(fun line -> supports_TLS line)
+let resultP1 = solve_p1 aoc_input
 let resultP2 = 0
