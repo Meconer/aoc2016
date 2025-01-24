0ff8da054c421273921100533ab19166b41ecfbf
Meconer <mats.asson@gmail.com>
2025-01-24 07:20:14 +0100

    wip

--- lib/day10/day10.ml
+++ lib/day10/day10.ml
@@ -6,5 +6,14 @@ let filename =
   if isExample then "lib/day9/example.txt" else "lib/day9/input.txt"
 
 let aoc_input = In_channel.read_lines filename
+
+let solve_p1 lines = 
+  let rec loop bots lines =
+    match lines with 
+    | []->bots
+    | line::rest ->
+      if String.is_prefix "value" then
+        let bot_no, value = Scanf
+
 let resultP1 = 0
 let resultP2 = 0
