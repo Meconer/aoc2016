342256580b4291bbbc58c06135706b9d9eae41b9
Meconer <mats.asson@gmail.com>
2025-01-25 22:24:45 +0100

    WIP

--- lib/day11/day11.ml
+++ lib/day11/day11.ml
@@ -3,9 +3,9 @@ open Core
 let isExample = false
 
 type elevator_t = { floor : int; contents : string list }
-type state_t = {floors:string list list; elevator: elevator_t}
+type state_t = { floors : string list list; elevator : elevator_t }
 
-let elevator = {floor= 0 ; contents = []}
+let elevator = { floor = 0; contents = [] }
 
 let floors =
   if isExample then [ [ "HYM"; "LIM" ]; [ "HYG" ]; [ "LIG" ]; [] ]
@@ -16,6 +16,21 @@ let floors =
       [];
       [];
     ]
+
+let string_of_elevator elevator =
+  string_of_int elevator.floor
+  ^ List.fold elevator.contents ~init:"" ~f:(fun acc el -> acc ^ "," ^ el)
+
+let string_of_floor floor floor_no =
+  string_of_int floor_no ^ "/"
+  ^ List.fold floor ~init:"" ~f:(fun acc el -> acc ^ el ^ "/")
+
+let string_of_floors floors =
+  List.foldi floors ~init:"" ~f:(fun idx acc floor ->
+      acc ^ string_of_floor floor idx)
+
+let string_of_state elevator floors =
+  string_of_elevator elevator ^ "|" ^ string_of_floors floors
 
 let is_mc s = Char.equal (String.get s 2) 'M'
 let is_gen s = Char.equal (String.get s 2) 'G'
@@ -44,13 +59,13 @@ let legal_floor floor =
     let sorted = List.sort floor ~compare:String.compare in
     if has_single_mc sorted && has_single_gen sorted then false else true
 
-let is_target state =
-  
-let solve_p1 floors elevator =
+(* let is_target state =
 
-  let rec loop step_count state =
-    if is_target state then step_count
-    else
+   let solve_p1 floors elevator =
 
-let resultP1 = 0
-let resultP2 = 0
+     let rec loop step_count state =
+       if is_target state then step_count
+       else
+
+   let resultP1 = 0
+   let resultP2 = 0 *)
