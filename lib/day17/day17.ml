c20d9e055f242121eb9fa70c8c136f1dc51c5bcb
Meconer <mats.asson@gmail.com>
2025-02-05 15:08:46 +0100

    Day 17 part 1 solved

--- lib/day17/day17.ml
+++ lib/day17/day17.ml
@@ -16,11 +16,14 @@ end
 
 (* Define the value module *)
 module State = struct
-  type t = coord
+  type t = coord * string
 
   let compare a b =
-    let cmp = Int.compare a.x b.x in
-    if cmp = 0 then Int.compare a.y b.y else cmp
+    let cmp = Int.compare (fst a).x (fst b).x in
+    if cmp = 0 then
+      let cmp' = Int.compare (fst a).y (fst b).y in
+      if cmp' = 0 then String.compare (snd a) (snd b) else cmp'
+    else cmp
 
   let equal a b = a.x = b.x && a.y = b.y
   let hash = Hashtbl.hash
@@ -43,33 +46,67 @@ let new_pos pos idx =
   | 3 -> { x = pos.x + 1; y = pos.y } (* Right *)
   | _ -> failwith "Illegal idx"
 
+let width = 4
+let height = 4
+
+let add_dir_to_path path idx =
+  let char_to_add =
+    match idx with
+    | 0 -> "U" (* Up *)
+    | 1 -> "D" (* Down *)
+    | 2 -> "L" (* Left *)
+    | 3 -> "R" (* Right *)
+    | _ -> failwith "Illegal idx"
+  in
+  path ^ char_to_add
+
 let get_neighbour_states path pos =
   let hash = get_hash path in
   let neighbours =
     String.sub hash ~pos:0 ~len:4
     |> String.to_list
-    |> List.mapi ~f:(fun idx ch -> (new_pos pos idx, is_open ch))
-    |> List.filter ~f:(fun (pos, is_open) ->
-           pos.x >= 0 && pos.x < 4 && pos.y >= 0 && pos.y < 4 && is_open)
+    |> List.mapi ~f:(fun idx ch ->
+           let new_path = add_dir_to_path path idx in
+           (new_pos pos idx, is_open ch, new_path))
+    |> List.filter ~f:(fun (pos, is_open, _) ->
+           pos.x >= 0 && pos.x < width && pos.y >= 0 && pos.y < height
+           && is_open)
   in
   neighbours
 
-(* let solve start target pass_code =
+let solve start target pass_code =
   let path = pass_code in
-  let queue = StatePSQ.( (add start 0) empty)  in
-    let rec loop queue =
-      if StatePSQ.is_empty queue then None
-      else
-        let popped = StatePSQ.pop queue in
-        match popped with 
-        | None -> failwith "Empty! Wtf?"
-        | Some ((pos, dist), queue) ->
-          if State.equal pos target then
-            path
+  let queue = StatePSQ.((add (start, path) 0) empty) in
+
+  let rec loop queue =
+    if StatePSQ.is_empty queue then None
+    else
+      let popped = StatePSQ.pop queue in
+      match popped with
+      | None -> failwith "Empty! Wtf?"
+      | Some (((pos, path), dist), queue) ->
+          if State.equal pos target then Some path
           else
-            let neighbours = get_neighbour_states path pos
-          in
+            let neighbours = get_neighbour_states path pos in
+            let q =
+              List.fold neighbours ~init:queue ~f:(fun q neighbour ->
+                  let new_pos, _, new_path = neighbour in
+                  StatePSQ.add (new_pos, new_path) (dist + 1) q)
+            in
+            loop q
+  in
 
+  loop queue
 
-let result_p1 = ""
-let result_p2 = "" *)
+let start = { x = 0; y = 0 }
+let target = { x = 3; y = 3 }
+
+let res =
+  match solve start target pass_code with
+  | Some res ->
+      let p_len = String.length pass_code in
+      String.sub res ~pos:p_len ~len:(String.length res - p_len)
+  | None -> "No result"
+
+let result_p1 = res
+let result_p2 = ""
