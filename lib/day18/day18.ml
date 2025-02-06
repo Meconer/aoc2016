6c800d2c37e69cb725556609fcf41c2aa0573c65
Meconer <mats.asson@gmail.com>
2025-02-06 13:10:58 +0100

    Day 18 completed

--- lib/day18/day18.ml
+++ lib/day18/day18.ml
@@ -1,11 +1,13 @@
-(* open Core *)
+open Core
 
-let is_example = true
+let is_example = false
 
 let first_line =
   if is_example then ".^^.^.^^^^"
   else
     "^..^^.^^^..^^.^...^^^^^....^.^..^^^.^.^.^^...^.^.^.^.^^.....^.^^.^.^.^.^.^.^^..^^^^^...^.....^....^."
+
+let no_of_lines_to_generate = if is_example then 10 else 40
 
 let get_next_line line =
   let len = String.length line in
@@ -15,11 +17,23 @@ let get_next_line line =
     | '^', '^', '.' | '.', '^', '^' | '^', '.', '.' | '.', '.', '^' -> '^'
     | _ -> '.'
   in
-  String.init len next_char
+  String.init len ~f:(fun i -> next_char i)
 
 let rec generate_lines n current_line =
   if n <= 1 then [ current_line ]
   else current_line :: generate_lines (n - 1) (get_next_line current_line)
 
-let result_p1 = 0
-let result_p2 = 0
+let no_of_safe_tiles_in_line line =
+  String.count line ~f:(fun ch -> Char.equal ch '.')
+
+let lines = generate_lines no_of_lines_to_generate first_line
+
+let result_p1 =
+  List.fold lines ~init:0 ~f:(fun acc line ->
+      acc + no_of_safe_tiles_in_line line)
+
+let lines = generate_lines 400000 first_line
+
+let result_p2 =
+  List.fold lines ~init:0 ~f:(fun acc line ->
+      acc + no_of_safe_tiles_in_line line)
