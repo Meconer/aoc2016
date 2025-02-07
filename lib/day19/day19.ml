b93a25ceaebf2d14e0467ffcc704c45b248d18cc
Meconer <mats.asson@gmail.com>
2025-02-07 21:47:51 +0100

    Day 19 part 1

--- lib/day19/day19.ml
+++ lib/day19/day19.ml
@@ -1,17 +1,18 @@
 open Core
 
-let is_example = true
-let no_of_elfs = if is_example then 11 else 3014387
+let is_example = false
+let no_of_elfs = if is_example then 17 else 3014387
 
 type node_t = { idx : int; count : int }
 
-let elfs = ref (Array.init no_of_elfs ~f:(fun idx -> { idx; count = 1 }))
+let elfs = ref (Array.init no_of_elfs ~f:(fun i -> { idx = i + 1; count = 1 }))
 
 let rec get_next_elfs_present start_pos pos =
   if pos = start_pos then None
   else
     let next_elf = !elfs.(pos) in
-    if next_elf.count = 0 then get_next_elfs_present start_pos (pos + 1)
+    if next_elf.count = 0 then
+      get_next_elfs_present start_pos ((pos + 1) mod no_of_elfs)
     else (
       Array.set !elfs pos { idx = pos; count = 0 };
       Some next_elf.count)
@@ -19,15 +20,15 @@ let rec get_next_elfs_present start_pos pos =
 let solve elfs start =
   let rec loop pos =
     let this_elf = elfs.(pos) in
-    Array.iter elfs ~f:(fun el ->
+    (* Array.iter elfs ~f:(fun el ->
         Printf.printf "idx %d, c: %d\n" el.idx el.count);
-    Out_channel.flush stdout;
-    let _ = In_channel.input_line In_channel.stdin in
+    Out_channel.flush stdout; *)
+    (* let _ = In_channel.input_line In_channel.stdin in *)
     if this_elf.count = 0 then loop ((pos + 1) mod no_of_elfs)
     else
       let next_count = get_next_elfs_present pos ((pos + 1) mod no_of_elfs) in
       match next_count with
-      | None -> pos
+      | None -> elfs.(pos).idx
       | Some next_count ->
           let this_elf =
             { this_elf with count = this_elf.count + next_count }
@@ -37,5 +38,5 @@ let solve elfs start =
   in
   loop start
 
-let result_p1 = 0
+let result_p1 = solve !elfs 0
 let result_p2 = 0
