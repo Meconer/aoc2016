0e2db5a1638a2c549b7ef78fdb1f5bda0e893840
Meconer <mats.asson@gmail.com>
2025-02-07 17:40:14 +0100

    wip

--- lib/day19/day19.ml
+++ lib/day19/day19.ml
@@ -2,5 +2,9 @@ open Core
 
 let is_example = false
 let no_of_elfs = if is_example then 5 else 3014387
+
+type node_t = { idx : int; count : int }
+
+let elfs = Array.init
 let result_p1 = 0
 let result_p2 = 0
