f4012d144d9673aba864117569df8d8a476c9372
Meconer <mats.asson@gmail.com>
2025-02-09 16:54:03 +0100

    Day 21 started

---
+++ lib/day21/day21.ml
@@ -0,0 +1,88 @@
+open Core
+
+let isExample = true
+
+let filename =
+  if isExample then "lib/day21/example.txt" else "lib/day21/input.txt"
+
+let password = if isExample then "abcde" else "abcdefgh"
+let aoc_input = In_channel.read_lines filename
+
+type commands_t =
+  | Swap_Pos
+  | Swap_Letter
+  | Rotate_Left
+  | Rotate_Right
+  | Rotate_Pos
+  | Reverse_Pos
+  | Move_Pos
+
+type command = {
+  command : commands_t;
+  pos_a : int;
+  pos_b : int;
+  letter_a : char;
+  letter_b : char;
+  no_steps : int;
+}
+
+let command_of_line line =
+  match line with
+  | line when String.is_prefix line ~prefix:"swap position" ->
+      let pos_a, pos_b =
+        Scanf.sscanf line "swap position %d with position %d" (fun a b ->
+            (a, b))
+      in
+      {
+        command = Swap_Pos;
+        pos_a;
+        pos_b;
+        letter_a = 'X';
+        letter_b = 'X';
+        no_steps = 0;
+      }
+  | line when String.is_prefix line ~prefix:"swap letter" ->
+      let letter_a, letter_b =
+        Scanf.sscanf line "swap letter %c with letter %c" (fun a b -> (a, b))
+      in
+      {
+        command = Swap_Letter;
+        pos_a = 0;
+        pos_b = 0;
+        letter_a;
+        letter_b;
+        no_steps = 0;
+      }
+  | line when String.is_prefix line ~prefix:"rotate left" ->
+      let no_steps = Scanf.sscanf line "rotate left %d step" (fun s -> s) in
+      {
+        command = Rotate_Left;
+        pos_a = 0;
+        pos_b = 0;
+        letter_a = 'X';
+        letter_b = 'X';
+        no_steps;
+      }
+  | line when String.is_prefix line ~prefix:"rotate right" ->
+      let no_steps = Scanf.sscanf line "rotate right %d step" (fun s -> s) in
+      {
+        command = Rotate_Right;
+        pos_a = 0;
+        pos_b = 0;
+        letter_a = 'X';
+        letter_b = 'X';
+        no_steps;
+      }
+  | line when String.is_prefix line ~prefix:"rotate left" ->
+      let no_steps = Scanf.sscanf line "rotate left %d step" (fun s -> s) in
+      {
+        command = Swap_Letter;
+        pos_a = 0;
+        pos_b = 0;
+        letter_a = 'X';
+        letter_b = 'X';
+        no_steps;
+      }
+
+let result_p1 = ""
+let result_p2 = ""
