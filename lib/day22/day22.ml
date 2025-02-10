d850da9ef400535aee94a7104a129bc3604591dd
Meconer <mats.asson@gmail.com>
2025-02-10 21:19:03 +0100

    day 22 wip

--- lib/day22/day22.ml
+++ lib/day22/day22.ml
@@ -3,8 +3,19 @@ open Core
 let isExample = false
 
 let filename =
-  if isExample then "lib/day21/example.txt" else "lib/day21/input.txt"
+  if isExample then "lib/day22/example.txt" else "lib/day22/input.txt"
 
 let aoc_input = In_channel.read_lines filename
+let aoc_input = List.sub aoc_input ~pos:2 ~len:(List.length aoc_input - 2)
+
+type node_t = { x : int; y : int; size : int; used : int }
+
+let get_node line =
+  let x, y, size, used =
+    Scanf.sscanf line "/dev/grid/node-x%d-y%d     %dT   %dT    %dT   %d"
+      (fun x y s u _ _ -> (x, y, s, u))
+  in
+  { x; y; size; used }
+
 let result_p1 = 0
 let result_p2 = 0
