01da801e13272d55b14db5f96e6f35ed1b826b19
Meconer <mats.asson@gmail.com>
2025-02-13 11:09:30 +0100

    Day 23 completed. I skipped optimizing part 2. Runtime is over 19 minutes

--- lib/day23/day23.ml
+++ lib/day23/day23.ml
@@ -145,4 +145,4 @@ let p1_regs = solve program { a = 7; b = 0; c = 0; d = 0 }
 let result_p1 = p1_regs.a
 let program = Array.of_list aoc_input
 let p2_regs = solve program { a = 12; b = 0; c = 0; d = 0 }
-let result_p2 = 0
+let result_p2 = p2_regs.a
